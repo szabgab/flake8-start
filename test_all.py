45d4f152547b79a0fd61c003ca1c672c77245633
Gabor Szabo <gabor@szabgab.com>
2021-09-28 10:31:11 +0300

    tests

---
+++ test_all.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import shutil
+import pytest
+
+@pytest.mark.parametrize("name", ['duplicate', 'unused_duplicate'])
+def test_duplicate(name, tmpdir):
+    print(f'{name} {tmpdir}')
+    shutil.copy(f'examples/{name}.py', f'{tmpdir}/{name}.py')
+    os.system(f'{sys.executable} flake8-start.py {tmpdir}')
+    with open(f'{tmpdir}/.flake8') as fh:
+        actual = fh.readlines()
+    with open(f'examples/{name}.flake8') as fh:
+        expected = fh.readlines()
+    assert actual == expected
